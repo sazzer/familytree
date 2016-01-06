69aa06bf0e9030999d4fab8daca5d3140220c5de
Graham Cox <graham@grahamcox.co.uk>
2016-01-06 08:04:07 +0000

    Set up Spring webapp

---
+++ webapp/src/main/kotlin/uk/co/grahamcox/familytree/webapp/spring/ControllersContext.kt
@@ -0,0 +1,11 @@
+package uk.co.grahamcox.familytree.webapp.spring
+
+import org.springframework.context.annotation.Configuration
+
+/**
+ * The configuration to use for the Spring MVC Controllers
+ */
+@Configuration
+open class ControllersContext {
+
+}
