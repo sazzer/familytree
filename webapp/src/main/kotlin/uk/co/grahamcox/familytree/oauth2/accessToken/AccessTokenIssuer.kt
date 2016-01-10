34949af049e49c26d5eee9837701eab4110b29d7
Graham Cox <graham@grahamcox.co.uk>
2016-01-10 17:46:24 +0000

    Generate a JWT as an Access Token

--- webapp/src/main/kotlin/uk/co/grahamcox/familytree/oauth2/accessToken/AccessTokenIssuer.kt
+++ webapp/src/main/kotlin/uk/co/grahamcox/familytree/oauth2/accessToken/AccessTokenIssuer.kt
@@ -1,5 +1,8 @@
 package uk.co.grahamcox.familytree.oauth2.accessToken
 
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.impl.crypto.MacProvider
 import uk.co.grahamcox.familytree.oauth2.Scopes
 import uk.co.grahamcox.familytree.oauth2.client.ClientDetails
 import java.time.Clock
@@ -13,6 +16,9 @@ import java.util.*
  */
 class AccessTokenIssuer(private val clock: Clock,
                         private val duration: Duration = Duration.ofHours(1)) {
+    /** The Key to use for signing JWTs */
+    private val jwtKey = MacProvider.generateKey()
+
     /**
      * Issue an Access Token for a specific client
      * @param client The client to issue the access token for
@@ -20,7 +26,21 @@ class AccessTokenIssuer(private val clock: Clock,
      * @return the access token
      */
     fun issueForClient(client: ClientDetails, scopes: Scopes?): AccessToken {
-        return AccessToken(accessTokenId = AccessTokenId(UUID.randomUUID().toString()),
+        val issuedAt = clock.instant()
+        val expiresAt = issuedAt.plus(duration)
+
+        val accessToken = Jwts.builder()
+            .setIssuer(AccessTokenIssuer::class.qualifiedName)
+            .setSubject(client.id.id)
+            .setAudience(client.id.id)
+            .setExpiration(Date.from(expiresAt))
+            .setNotBefore(Date.from(issuedAt))
+            .setIssuedAt(Date.from(issuedAt))
+            .setId(UUID.randomUUID().toString())
+            .signWith(SignatureAlgorithm.HS512, jwtKey)
+            .compact()
+
+        return AccessToken(accessTokenId = AccessTokenId(accessToken),
                 refreshTokenId = null,
                 expires = clock.instant().plus(duration),
                 scopes = client.scopes)
